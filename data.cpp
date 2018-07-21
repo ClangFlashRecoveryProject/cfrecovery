b77bc427e269796a8589523874404c5773f1ca97
clangflashme <clangflashme@gmail.com>
2018-07-21 12:22:47 +0400

    skip digest check and generating by default

--- data.cpp
+++ data.cpp
@@ -732,8 +732,8 @@ void DataManager::SetDefaultValues()
 	mPersist.SetValue(TW_TIME_ZONE_VAR, "CST6CDT,M3.2.0,M11.1.0");
 	mPersist.SetValue(TW_GUI_SORT_ORDER, "1");
 	mPersist.SetValue(TW_RM_RF_VAR, "0");
-	mPersist.SetValue(TW_SKIP_DIGEST_CHECK_VAR, "0");
-	mPersist.SetValue(TW_SKIP_DIGEST_GENERATE_VAR, "0");
+	mPersist.SetValue(TW_SKIP_DIGEST_CHECK_VAR, "1");
+	mPersist.SetValue(TW_SKIP_DIGEST_GENERATE_VAR, "1");
 	mPersist.SetValue(TW_SDEXT_SIZE, "0");
 	mPersist.SetValue(TW_SWAP_SIZE, "0");
 	mPersist.SetValue(TW_SDPART_FILE_SYSTEM, "ext3");
